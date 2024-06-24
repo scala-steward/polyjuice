e4f31b42d023e288808d447c66684fb2f63f0274
Scala Steward <scala_steward@virtuslab.com>
2024-06-24 19:05:45 +0000

    Update scalatest to 3.2.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val circeVersion = "0.12.1"
   lazy val http4sVersion = "0.18.24"
   lazy val logbackVersion = "1.2.3"
-  lazy val scalaTestVersion = "3.2.0"
+  lazy val scalaTestVersion = "3.2.19"
   lazy val typesafeConfigVersion = "1.4.0"
   lazy val typesafeLoggingVersion = "3.9.2"
 
